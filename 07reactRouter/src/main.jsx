36fe4fd43b44c063b3f3297c8faff0436931bb60
Shubranshu Shekhar <shubranshumishra22@gmail.com>
2024-05-04 11:16:16 +0530

    router basic done

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,27 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import ContactUs from './components/ContactUs/ContactUs.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-      path: "",
-      element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//       path: "",
+//       element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contactus',
+//         element: <ContactUs />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />}/>
+      <Route path='contactus' element={<ContactUs />}/>
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
