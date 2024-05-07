cd3c6a1c071b6aaa267f6c4417a790c3d26a5bfc
Shubranshu Shekhar <shubranshumishra22@gmail.com>
2024-05-07 10:06:44 +0530

    theme mode project started

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { ThemeProvider } from './Context/theme'
+
+function App() {
+  const [themeMode, setthemMode] = useState('light')
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* {theme button} */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* {Card} */}
+                    </div>
+                </div>
+    </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
